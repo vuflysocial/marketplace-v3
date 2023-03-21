4b6d0745efbf68cef3a4536c8164139b59372b06
meloinu <meloinuofficial@gmail.com>
2023-03-21 13:31:06 -0700

    dammit

--- pages/chart.tsx
+++ pages/chart.tsx
@@ -8,10 +8,10 @@ import Container from "../components/Container/Container";
 function Chart() {
   return (
     <Container maxWidth="lg">
-      <Link className={styles.heroCta} href="/">
+      <Link className={styles.heroCta} href="https://www.dextools.io/app/en/bnb/pair-explorer/0x82c6390b7a5a62a2ecf88116b34dda7a1f82d411">
         <div>BSC | Melo Inu</div>
       </Link>
-      <Link className={styles.heroCta} href="/">
+      <Link className={styles.heroCta} href="https://www.dextools.io/app/en/canto/pair-explorer/0x0958093f4bbf679e5898a46215d2ecb400ce4c67">
         <div>CANTO | Melo Inu</div>
       </Link>
     </Container>
