b08ac91cb8934b333e3afc44f619d2d9cc4db226
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-04-01 09:31:40 -0700

    Update Eco.tsx

--- pages/Eco.tsx
+++ pages/Eco.tsx
@@ -1,13 +1,29 @@
 import { useState, useEffect } from 'react';
 import Container from '../components/Container/Container';
-
 import NextLink from 'next/link';
-import Image from 'next/image'
+import Image from 'next/image';
 
+const CountdownTimer = () => {
+  const [timeLeft, setTimeLeft] = useState(57600); // in seconds
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft((prevTime) => prevTime - 1);
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
 
+  const progress = ((57600 - timeLeft) / 57600) * 100; // percentage
+
+  return (
+    <div>
+      <div>{timeLeft} seconds left</div>
+      <progress value={progress} max="100" />
+    </div>
+  );
+}
 
 function Eco() {
-  const [posts, setPosts] = useState<[]>([]);
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     // Fetch posts from API or database and update state
@@ -19,10 +35,8 @@ function Eco() {
   }, []);
 
   return (
-    
     <Container maxWidth="lg">
       <div><h1>Token Ecosytem</h1></div>
-      
       <NextLink href="https://poocoin.app/tokens/0x5c12c812794b874fe4fdea9a4960df599c89b8e5">
         <div style={{ display: "flex", alignItems: "center" }}>
           <button style={{ marginRight: "10px" }}>Melo Inu</button>
@@ -30,7 +44,6 @@ function Eco() {
         </div>
       </NextLink>
       <br />
-     
       <NextLink href="/">
         <div style={{ display: "flex", alignItems: "center" }}>
           <button style={{ marginRight: "10px" }}>Kami Shinto</button>
@@ -38,7 +51,6 @@ function Eco() {
         </div>
       </NextLink>
       <br />
-     
       <NextLink href="/">
         <div style={{ display: "flex", alignItems: "center" }}>
           <button style={{ marginRight: "10px" }}>...</button>
@@ -46,29 +58,29 @@ function Eco() {
         </div>
       </NextLink>
       <br />
-     
       <NextLink href="/">
         <div style={{ display: "flex", alignItems: "center" }}>
           <button style={{ marginRight: "10px" }}>...</button>
           <Image src="" alt="Melo Inu" width={100} height={100} />
         </div>
-        <br />
-        <br />
-          <h1>KAMI SHINTO | SPIRIT</h1>
-          <button style={{ fontSize: '20px', padding: '10px 20px', borderRadius: '5px', backgroundColor: '#FF0000', color: 'white' }} onClick={() => window.location.href='https://www.example.com'}>
-    Chart
-  </button>
-        <div>
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-    <img src="/R.gif" alt="your-image-description" />
-  </div>
+      </NextLink>
+      <br />
+      <CountdownTimer /> {/* added countdown timer */}
+      <br />
       
-        </div>
-        <div>
-          <br />
-          
-
-          <h1>The concept of kami is hard to explain.</h1>
+        <h1>KAMI SHINTO | SPIRIT</h1>
+      <button style={{ fontSize: '20px', padding: '10px 20px', borderRadius: '5px', backgroundColor: '#FF0000', color: 'white' }} onClick={() => window.location.href='https://www.example.com'}>
+        Chart
+      </button>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <img src="/R.gif" alt="Your Image Description" />
+      </div>
+      <div>
+        
+      </div>
+      <br />
+      <div>
+      <h1>The concept of kami is hard to explain.</h1>
     <p>Shintoists would say that this is because human beings are simply incapable of forming a true understanding of the nature of kami. To make understanding easier kami are often described as divine beings, as spirits or gods. But kami are not much like the gods of other faiths:</p>
     
 <h5>Kami are not divine like the transcendent and omnipotent deities found in many religions.</h5>
@@ -80,12 +92,10 @@ function Eco() {
 <h5>Some beings themselves.</h5>
 <h5>Elements of the landscape, like mountains and lakes.</h5>
 <h5>Powerful forces of nature, like storms and earthquakes.</h5>
-
-        </div>
-        <div>
-          <br />
-
-          <div>
+      </div>
+      <br />
+      <div>
+      <div>
         <h1>Shintonomics </h1>
         
         <h2>Token Information</h2>
@@ -113,12 +123,13 @@ function Eco() {
 
         </ul>
       </div>
-        </div>
-      </NextLink>
+      </div>
     </Container>
-    
   );
 }
 
 export default Eco;
+
+
+
 
