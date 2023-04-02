d78ea9f8ce49e0a4f8bdd6124d8dd19be57af7db
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-04-01 23:32:19 -0700

    Update dashboard.tsx

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,52 +1,53 @@
 import { NextPage } from "next";
-
 import styles from '../styles/Home.module.css';
-
-import Link from 'next/link'
+import Link from 'next/link';
 import Container from "../components/Container/Container";
+
 
 function Dashboard() {
   return (
     <Container maxWidth="lg">
-      <Link className={styles.heroCta} href="https://meloai.netlify.app/">
-        <div>Generate AI Images & use MeloGPT</div>
-      </Link>
-      <Link className={styles.heroCta} href="https://like-melo-mint.netlify.app/">
-        <div>Like Melo MINT</div>
-      </Link>
-      <Link className={styles.heroCta} href="https://like-melo-nft-gate.vercel.app/login">
-        <div>NFT GATED DAPP</div>
-      </Link>
-      <Link className={styles.heroCta} href="https://staking-app-melo.herokuapp.com/">
-        <div>STAKING DAPP</div>
-      </Link>
-      <Link className={styles.heroCta} href="https://twitter.com/meloinu">
-        <div>Twitter</div>
-      </Link>
-      <Link className={styles.heroCta} href="https://t.me/meloinuofficial">
-        <div>Telegram</div>
-      </Link>
-      <Link className={styles.heroCta} href="https://melo-inu.gitbook.io/whitepaper/">
-        <div>WhitePaper V2</div>
-      </Link>
-      <Link className={styles.heroCta} href="/tokenomics">
-        <div>Tokenomics</div>
-      </Link>
-      <Link className={styles.heroCta} href="/music">
-        <div>Music</div>
-      </Link>
-      <Link className={styles.heroCta} href="/buy">
-        <div>Buy NFTs</div>
-      </Link>
-      <Link className={styles.heroCta} href="/sell">
-        <div>Sell NFTs</div>
-      </Link>
-      <Link className={styles.heroCta} href="/create">
-        <div>Create</div>
-      </Link>
+      <div className={styles.buttonsContainer}>
+        <div className={styles.buttons}>
+          <Link className={styles.heroCta} href="https://meloai.netlify.app/">
+            <div>Generate AI Images & use MeloGPT</div>
+          </Link>
+          <Link className={styles.heroCta} href="https://like-melo-mint.netlify.app/">
+            <div>Like Melo MINT</div>
+          </Link>
+          <Link className={styles.heroCta} href="https://like-melo-nft-gate.vercel.app/login">
+            <div>NFT GATED DAPP</div>
+          </Link>
+          <Link className={styles.heroCta} href="https://staking-app-melo.herokuapp.com/">
+            <div>STAKING DAPP</div>
+          </Link>
+          <Link className={styles.heroCta} href="https://twitter.com/meloinu">
+            <div>Twitter</div>
+          </Link>
+          <Link className={styles.heroCta} href="https://t.me/meloinuofficial">
+            <div>Telegram</div>
+          </Link>
+          <Link className={styles.heroCta} href="https://melo-inu.gitbook.io/whitepaper/">
+            <div>WhitePaper V2</div>
+          </Link>
+          <Link className={styles.heroCta} href="/melonomics">
+            <div>Melonomics</div>
+          </Link>
+          <Link className={styles.heroCta} href="/music">
+            <div>Music</div>
+          </Link>
+          
+        
+        </div>
+        <div className={styles.video}>
+          <video width="400" height="300" controls>
+            <source src="meloshi.mp4" type="video/mp4" />
+          </video>
+        </div>
+      </div>
     </Container>
   )
 }
 
-
 export default Dashboard;
+
