1666b686b053ed3f242c283ea740677d5a8c6756
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-04-04 08:16:49 -0700

    Update rpc.tsx

--- pages/rpc.tsx
+++ pages/rpc.tsx
@@ -1,28 +1,22 @@
 import { useState } from 'react';
 import Container from '../components/Container/Container';
 import NextLink from 'next/link';
-import styles from '../styles/tokenomics.module.css';
+import styles from "../styles/tokenomics.module.css";
 
-const Rpc = () => {
+const RpcContainer = () => {
   const [rpcEndpoint, setRpcEndpoint] = useState('https://node-01-eba5.settlemint.com'); // Replace with your JSON-RPC endpoint
   const [chainId, setChainId] = useState('40000'); // Replace with your chain ID
+  const [currencySymbol, setCurrencySymbol] = useState('MELO');
 
   return (
-    
     <Container maxWidth="lg">
-        <div>
-
-        </div>
-        <div>
-            <br />
-            
-        </div>
-      <div>
-        <h2>Add Meloreum Network Testnet</h2>
+      <div style={{ backgroundColor: 'green', color: 'black', padding: '20px', borderRadius: '10px', fontFamily: 'Nunito Sans Bold' }}>
+        <h2>Add Meloreum Test Network Information</h2>
         <p>JSON-RPC Endpoint: {rpcEndpoint}</p>
         <p>Chain ID: {chainId}</p>
+        <p>Currency Symbol: {currencySymbol}</p>
         <button onClick={() => {
-          navigator.clipboard.writeText(`${rpcEndpoint},${chainId}`);
+          navigator.clipboard.writeText(`${rpcEndpoint},${chainId},${currencySymbol}`);
           alert('Network information copied to clipboard!');
         }}>Copy to Clipboard</button>
       </div>
@@ -30,4 +24,5 @@ const Rpc = () => {
   );
 };
 
-export default Rpc;
+export default RpcContainer;
+
