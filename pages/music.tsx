1be297d1bc8c328e739f4a6a0089c10edae62bb7
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-04-04 08:39:23 -0700

    Update music.tsx

--- pages/music.tsx
+++ pages/music.tsx
@@ -2,12 +2,20 @@ import { NextPage } from "next";
 import styles from '../styles/music.module.css';
 import Container from "../components/Container/Container";
 
+import Link from "next/link"
+
 function Music() {
   return (
     <Container maxWidth="lg">
+
       <div>
         <div>
           <h3>Melos Favorite Artists</h3>
+        </div>
+        <div>
+      <Link href="/dashboard">
+        <div className={styles.heroCta}>Back To Dashboard</div>
+      </Link>
         </div>
 
         <div>
@@ -25,6 +33,7 @@ function Music() {
           <source src="audio.mp3" type="audio/mpeg" />
           Your browser does not support the audio element.
         </audio>
+     
       </div>
       <br />
       <div>
@@ -38,12 +47,14 @@ function Music() {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       />
-      <audio controls>
-        <source src="audio.mp3" type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
+      <div>
+
+      </div>
+    
+      
     </Container>
   );
 }
 
 export default Music;
+
