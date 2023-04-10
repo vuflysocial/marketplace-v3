381d712546fd83aa47cf7eb44e22997f01ea900b
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-04-09 22:08:43 -0700

    Create thestory.tsx

---
+++ pages/thestory.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+import styles from '../styles/tokenomics.module.css';
+import Container from "../components/Container/Container";
+
+function thestory() {
+  return (
+    <Container maxWidth="lg">
+      <div>
+        <h1>The Original Story</h1>
+        
+      </div>
+    </Container>
+  );
+}
+
+export default thestory;
